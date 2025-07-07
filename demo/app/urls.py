b0d2ae6d5042969cd4c28e644f51444c57610353
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-07 18:14:35 +0530

    Router Written

--- demo/app/urls.py
+++ demo/app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
-urlpatterns = [
-    
-]
+from rest_framework.routers import DefaultRouter
+from app.views import CrudVS
+router = DefaultRouter()
+router.register(r'crud',CrudVS,basename='crud')
+urlpatterns = router.urls
